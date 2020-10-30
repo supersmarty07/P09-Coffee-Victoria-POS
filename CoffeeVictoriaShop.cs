5c5c7f09a79a750eb01aef508ef1d27f3d59a756
supersmarty07 <elohistsachet007@gmail.com>
2020-10-30 23:26:17 +1100

    Add project files.

---
+++ CoffeeVictoriaShop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coffee_Victoria_POS
+{
+    class CoffeeVictoriaShop
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coffee_Victoria_POS
+{
+    class Product
+    {
+    }
+}
